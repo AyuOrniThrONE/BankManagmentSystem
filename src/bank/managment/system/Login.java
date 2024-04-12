6579b03fd503a8fa0cbd2d78b9162498c7ff3a09
AyuOrniThrONE <ayushprakashthaware@gmail.com>
2024-04-12 20:07:36 +0530

    Added Transaction Page

--- src/bank/managment/system/Login.java
+++ src/bank/managment/system/Login.java
@@ -1,5 +1,6 @@
 package bank.managment.system;
 import java.awt.event.*;
+import java.sql.*;
 import java.awt.*;
 
 import javax.swing.*;
@@ -76,6 +77,23 @@ public class Login extends JFrame implements ActionListener{
 			cardTextField.setText("");
 			pinTextField.setText("");
 		}else if(ae.getSource()==login) {
+			Conn conn=new Conn();
+			String cardnumber=cardTextField.getText();
+			String pinnumber=pinTextField.getText();
+			
+			String query="select * from login where cardnumber = '"+cardnumber+"' and pin = '"+pinnumber+"'";
+			try {
+				ResultSet rs=conn.s.executeQuery(query);
+				if(rs.next()) {
+					setVisible(false);
+					new Transactions(pinnumber).setVisible(true);
+				}else {
+					JOptionPane.showMessageDialog(null, "Incorrect Card number or Pin");
+				}
+					
+			}catch(Exception e) {
+				System.out.println(e);
+			}
 			
 		}else if(ae.getSource()==signup) {
 			setVisible(false);
